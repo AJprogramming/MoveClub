4ad846bbe91f4017e277612d6729dce6f3e6da57
Alec Arteaga <arteagajacob@gmail.com>
2023-05-31 10:01:55 -0700

    deleted config.py

--- scraped_movies/config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-connect = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='root',
-    database='movieclub'
-)
-
-driver_path = r'C:\Users\artea\Downloads\chromedriver_win32\chromedriver'
