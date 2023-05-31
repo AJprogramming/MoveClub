dc36d36f63542e999778e1dec25cad819b7859fc
Alec Arteaga <arteagajacob@gmail.com>
2023-05-31 09:53:34 -0700

    fixed table and added cr to server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from movieclubs.controllers import login_cr, home_cr, showclubs_cr, createclub_cr
+from movieclubs.controllers import login_cr, home_cr, showclubs_cr, createclub_cr, scraped_cr
 from movieclubs import app
 
 if __name__ == "__main__":
